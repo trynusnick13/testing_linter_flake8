fd4c54f1ef8ca49767cc57c00babe89a2f47db55
Liza <trynuspoc@gmail.com>
2020-10-14 14:11:15 +0300

    problem

--- test.py
+++ test.py
@@ -4,4 +4,4 @@ b=1
 c=2
 def gf():
 	pass
-a='5ssdsssxsaxsxxwsxdwxswxwxqsxwqxswxdxefr4f4frt4gvrtbghttrbhthbhtyhneywtrtfvrvfrvfrecffvregvrevc4fevtg45yYREqd4eadrghbtbyjunrib5 jnikjuhygtfrde'
+a='5ssdsssxsaxsxxwsxdwxswxgvrtbghttrbhthbhtyhneywtrtfvrvfrvfrecffvregvrevc4fevtg45yYREqd4eadrghbtbyjunrib5 jnikjuhygtfrde'
