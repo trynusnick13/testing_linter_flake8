3a245a51d2aa091efe02ea41a288efcc5fbe1086
Liza <trynuspoc@gmail.com>
2020-10-14 14:03:20 +0300

    not pep8 file

---
+++ test.py
@@ -0,0 +1,7 @@
+import os
+a=1
+b=1
+c=2
+def gf():
+	pass
+a='5ssdsssxsaxsxxwsxdwxswxwxqsxwqxswxdxefr4f4frt4gvrtbghttrbhthbhtyhneywtrtfvrvfrvfrecffvregvrevc4fevtg45yYREqd4eadrghbtbyjunrib5 jnikjuhygtfrde'
